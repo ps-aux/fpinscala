415f2c178fa381e424a411f525bc3fa3360bb88f
ps-aux <seriouz.accz@gmail.com>
2016-12-17 18:19:43 +0100

    Add exercise 2.1 solutionn

--- src/main/scala/MyModule.scala
+++ src/main/scala/MyModule.scala
@@ -14,14 +14,30 @@ object MyModule {
   def factorial(n: Int): Int = {
     def go(n: Int, acc: Int): Int = {
       if (n == 0) acc
-      else go (n - 1, acc * n)
+      else go(n - 1, acc * n)
     }
 
     go(n, 1)
   }
 
-  def main(args: Array[String]): Unit =
+  /**
+    * Exercise 2.1
+    * Calculates the n-th Fibonacci number
+    */
+  def fibonacci(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, prev: Int, prev2: Int): Int = {
+      if (n == 0) prev
+      else go(n - 1, prev2, prev + prev2)
+    }
+
+    go(n, 0, 1)
+  }
+
+  def main(args: Array[String]): Unit = {
     println(formatAbs(-42))
     println(factorial(5))
+    println(fibonacci(6))
+  }
 
 }
