2cfe52d34f6a75fab25ef0cd18ed3a43b862875b
Arkonix <seriouz.accz@gmail.com>
2016-12-25 01:30:30 +0100

    Add some chapter 3 exercises

--- src/main/scala/fpinscala/datastructures/List.scala
+++ src/main/scala/fpinscala/datastructures/List.scala
@@ -25,7 +25,55 @@ object List {
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
 
+  /**
+    * Exercise 3.2
+    */
+  def tail[A](list: List[A]): List[A] = {
+    list match {
+      case Nil => Nil
+      case Cons(_, xs) => xs
+    }
+  }
 
-  def main(args: Array[String]) {
+  /**
+    * Exercise 3.3
+    */
+  def setHead[A](list: List[A], head: A): List[A] = {
+    list match {
+      case Nil => List(head)
+      case Cons(_, xs) => Cons(head, xs)
+    }
+  }
+
+  /**
+    * Exercise 3.4
+    */
+  def drop[A](list: List[A], count: Int): List[A] = {
+    if (count == 0)
+      list
+    else list match {
+      case Nil => Nil
+      case Cons(x, xs) => drop(xs, count - 1)
+    }
+  }
+
+  /**
+    * Exercise 3.5
+    */
+  def dropWhile[A](list: List[A], f: A => Boolean): List[A] = {
+    list match {
+      case Nil => Nil
+      case Cons(x, xs) => if (!f(x)) list else dropWhile(xs, f)
+    }
+  }
+
+
+  def main(args: Array[String]): Unit = {
+    val l = List(1, 2, 3, 4)
+
+    println(tail(l))
+    println(setHead(l, 55))
+    println(drop(l, 2))
+    println(dropWhile(l, (x: Int) => x < 4))
   }
 }
